662951a5a81c4f8ca94ad8d96e66fb3b257f3ec4
NoLogig <max-schweigerdt@web.de>
2018-10-31 23:46:44 +0100

    Add math helper

--- src/app/services/math/math.service.ts
+++ src/app/services/math/math.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class MathService {
 
   constructor() { }
+
+  /** Lock a value to Min/Max.
+   * @description Check whether a value has gone off Min/Max (canvas edges).
+   *     If so, it makes the value wrap around to the opposite.
+   * @example -  Min  Val   Max
+   *            -5    x    23
+   *       Max ←|____|____|→ Min
+   */
+  lock(n: number, min: number, max: number): number {
+    if (n < min) { return max; }
+    if (n > max) { return min; }
+    return n;
+  }
+
 }
